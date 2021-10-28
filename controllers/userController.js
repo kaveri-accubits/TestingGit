0bc5a40a108e77fae6a7bfad0f198c38ac93c55d
KaveriTS <tscauvery007@gmail.com>
2021-10-28 17:33:11 +0530

    added new folder utils

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,36 +4,35 @@ const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 //const userRoute = require("../routes/userRoute");
 
-
 //register route
-exports.register = (req, res, next) => {
-    try {
-      const { username, email, password } = req.body;
-      const newUser = new User({
-        username: username,
-        email: email,
-        password: hashPassword(password),
-      });
-  
-      newUser.save().then((result) => {
-          res.status(201).json({
-            message: "User created",
-            result: result,
-          });
-        }).catch((err) => {
-          res.status(500).json({
-            error: err,
-          });
-        });
-  
-      res.status(200).json({
-        message: "User registered successfully",
-        user: newUser,
-      });
-    }catch (error) {
-      res.status(500).json({
-        message: "Error registering user",
-        error,
-      });
-    }
-    };
+// exports.register = (req, res, next) => {
+//     try {
+//       const { username, email, password } = req.body;
+//       const newUser = new User({
+//         username: username,
+//         email: email,
+//         password: hashPassword(password),
+//       });
+
+//       newUser.save().then((result) => {
+//           res.status(201).json({
+//             message: "User created",
+//             result: result,
+//           });
+//         }).catch((err) => {
+//           res.status(500).json({
+//             error: err,
+//           });
+//         });
+
+//       res.status(200).json({
+//         message: "User registered successfully",
+//         user: newUser,
+//       });
+//     }catch (error) {
+//       res.status(500).json({
+//         message: "Error registering user",
+//         error,
+//       });
+//     }
+//     };
